6c6405fe76a3cee31cd75a895fb4b01d4671fdad
Johann Woelper <woelper@gmail.com>
2019-09-17 21:07:14 +0200

    build script fixes

--- build.rs
+++ build.rs
@@ -1,8 +1,10 @@
 #[cfg(windows)]
 extern crate windres;
+#[cfg(windows)]
 use windres::Build;
 
 fn main() {
+    #[cfg(windows)]
     match Build::new().compile("winres.rc") {
         Ok(_b) => println!("Made icon"),
         Err(e) => println!("{:?}", e)
