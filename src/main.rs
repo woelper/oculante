a7f6c73ddc485b0bb239096a2b44641e34cca8d2
Johann Woelper <woelper@gmail.com>
2020-05-31 11:08:36 +0200

    wip exr support

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ use gif_dispose;
 
 extern crate exr;
 use exr::prelude::*;
+use exr::prelude::simple_image::*;
+use exr::prelude::rgba_image as rgb_exr;
 
 use piston_window::*;
 use std::sync::mpsc;
@@ -124,10 +126,46 @@ fn open_image(img_location: &PathBuf, texture_sender: Sender<image_crate::RgbaIm
                 },
                 Some("exr") => {
 
-                    // let mut image = rgba::Image::read_from_file(&img_location, read_options::high()).unwrap();
-                    // let buffer: image_crate::RgbaImage = image_crate::ImageBuffer::from_raw(image.resolution.0 as u32, image.resolution.1 as u32, image).unwrap();
 
-                    // println!("loaded image {:#?}", image);
+                    // read the image from a file and keep only the png buffer
+                    let (_info, png_buffer) = rgb_exr::ImageInfo::read_pixels_from_file(
+                        &img_location,
+                        rgb_exr::read_options::high(),
+
+                        // how to create an empty png buffer from exr image meta data (used for loading the exr image)
+                        |info: &rgb_exr::ImageInfo| -> image_crate::RgbaImage {
+                            image_crate::ImageBuffer::new(
+                                info.resolution.width() as u32,
+                                info.resolution.height() as u32
+                            )
+                        },
+
+                        // set each pixel in the png buffer from the exr file
+                        |png_pixels: &mut image_crate::RgbaImage, position: rgb_exr::Vec2<usize>, pixel: rgb_exr::Pixel| {
+                            png_pixels.put_pixel(
+                                position.x() as u32, position.y() as u32,
+
+                                image_crate::Rgba([
+                                    tone_map(pixel.red.to_f32()),
+                                    tone_map(pixel.green.to_f32()),
+                                    tone_map(pixel.blue.to_f32()),
+                                    (pixel.alpha_or_default().to_f32() * 255.0) as u8,
+                                ])
+                            );
+                        },
+                    ).unwrap();
+
+
+                    /// compress any possible f32 into the range of [0,1].
+                    /// and then convert it to an unsigned byte.
+                    fn tone_map(linear: f32) -> u8 {
+                        // TODO does the `image` crate expect gamma corrected data?
+                        let clamped = (linear - 0.5).tanh() * 0.5 + 0.5;
+                        (clamped * 255.0) as u8
+                    };
+
+
+                    let _ = texture_sender.send(png_buffer);
     
     
                 },
