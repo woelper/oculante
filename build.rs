00039274bdb7cb8a44210e3dd0892e66d2cfcece
Johann Woelper <woelper@gmail.com>
2020-06-13 18:26:08 +0200

    larger icon

--- build.rs
+++ build.rs
@@ -4,9 +4,22 @@ extern crate windres;
 use windres::Build;
 
 fn main() {
+    
     #[cfg(windows)]
-    match Build::new().compile("winres.rc") {
-        Ok(_b) => println!("Made icon"),
+    match std::process::Command::new("convert")
+    .args(vec!["res/logo.png", "icon.ico"]).spawn(){
+        Ok(_b) => println!("Converted icon"),
         Err(e) => println!("{:?}", e)
     }
+
+
+    #[cfg(windows)]
+    match Build::new().compile("winres.rc") {
+        Ok(_b) => println!("Made icon ressource file"),
+        Err(e) => println!("{:?}", e)
+    }
+
+
+
+
 }
