4b72efbbee0ffbe5ebdf4668b21c61a3c55bd564
Johann Woelper <woelper@gmail.com>
2021-03-05 12:46:29 +0100

    update mac build

--- build_mac.sh
+++ build_mac.sh
@@ -1,3 +1,4 @@
-cargo bundle
-cp res/info.plist target/debug/bundle/osx/oculante.app/Contents/Info.plist
+cargo bundle --release
+#cargo bundle
+#cp res/info.plist target/debug/bundle/osx/oculante.app/Contents/Info.plist
 cp res/info.plist target/release/bundle/osx/oculante.app/Contents/Info.plist
