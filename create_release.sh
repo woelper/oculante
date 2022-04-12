d0c2015668c5c54074840738cc2778061af08a25
Johann Woelper <woelper@gmail.com>
2022-04-12 23:35:03 +0200

    release script

--- create_release.sh
+++ create_release.sh
@@ -3,5 +3,8 @@ cargo bump patch --git-tag
 #VERSION=`cargo pkgid | cut -d# -f2 | cut -d: -f2`
 #echo $VERSION
 git add Cargo.toml
+cargo update
+cargo check
+git add Cargo.lock
 git push --tags
 git push
