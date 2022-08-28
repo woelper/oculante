5765d2afeeeb7e6b306afd825301929b0fc73b91
Johann Woelper <woelper@gmail.com>
2022-08-28 20:45:57 +0200

    update build script

--- create_release.sh
+++ create_release.sh
@@ -1,11 +1,11 @@
 # This is a script to be locally run in order to release.
 cargo install cargo-bump
+cargo install cargo-get
 cargo bump patch --git-tag
 git add Cargo.toml
-cargo update
-cargo check
 git add Cargo.lock
-git commit -m "Bumped version"
+cargo check
+git commit -m "Release version `cargo get version`"
 git push --tags
 git push
 # this needs no-verify as we modify the plist during the build, and cargo does not accept that.
