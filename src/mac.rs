4d852cc06ec47bc24027fe8ff3d0505dc2e859aa
Johann Woelper <woelper@gmail.com>
2023-01-02 11:10:26 +0100

    Revert "chore: update clap"
    This reverts commit c08f5f1a47f35262d0fbfdcba8b1e0254c49c8ce.

--- src/mac.rs
+++ src/mac.rs
@@ -33,7 +33,7 @@ pub fn launch() -> Result<(), Box<dyn Error>> {
         .arg(
             Arg::new("chainload")
                 .required(false)
-                .num_args(0)
+                .takes_value(false)
                 .short('c')
                 .help("Chainload on Mac"),
         )
@@ -42,8 +42,7 @@ pub fn launch() -> Result<(), Box<dyn Error>> {
     debug!("Completed argument parsing.");
     let maybe_img_location = matches.get_one::<String>("INPUT").map(|arg| PathBuf::from(arg));
 
-    // contains_id("chainload")?
-    if !matches.contains_id("chainload") && maybe_img_location.is_none() {
+    if !matches.is_present("chainload") && maybe_img_location.is_none() {
         info!("Chainload not specified, and no input file present. Invoking mac hack.");
     } else {
         return Ok(());
