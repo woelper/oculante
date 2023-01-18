408f30acd216aea431dc5bc329a64a0279b6475e
Johann Woelper <woelper@gmail.com>
2023-01-18 22:40:34 +0100

    add jpgxl

--- src/utils.rs
+++ src/utils.rs
@@ -43,7 +43,7 @@ use crate::shortcuts::{lookup, InputEvent, Shortcuts};
 pub const SUPPORTED_EXTENSIONS: &'static [&'static str] = &[
     "bmp", "dds", "exr", "ff", "gif", "hdr", "ico", "jpeg", "jpg", "png", "pnm", "psd", "svg",
     "tga", "tif", "tiff", "webp", "nef", "cr2", "dng", "mos", "erf", "raf", "arw", "3fr", "ari",
-    "srf", "sr2", "braw", "r3d", "nrw", "raw", "avif",
+    "srf", "sr2", "braw", "r3d", "nrw", "raw", "avif", "jxl"
 ];
 
 fn is_pixel_fully_transparent(p: &Rgba<u8>) -> bool {
@@ -787,6 +787,15 @@ pub fn open_image(img_location: &PathBuf) -> Result<FrameCollection> {
             // make it a Dynamic image
             let d = DynamicImage::ImageRgb8(x);
             col.add_still(d.to_rgba8());
+        }
+        #[cfg(feature = "jpgxl")]
+        "jxl" => {
+            use jpegxl_rs::decoder_builder;
+            use jpegxl_rs::image::ToDynamic;
+            let sample = std::fs::read(&img_location)?;
+            let decoder = decoder_builder().build()?;
+            let img = decoder.decode(&sample)?.into_dynamic_image().context("Can't decode image from jpgxl")?;
+            col.add_still(img.into_rgba8());
         }
         "hdr" => {
             let f = File::open(&img_location)?;
