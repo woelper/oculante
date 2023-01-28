8762360fc4ee19bf53be63f1d176a93bc3d54934
Johann Woelper <woelper@gmail.com>
2023-01-28 22:40:22 +0100

    optional jxl

--- src/utils.rs
+++ src/utils.rs
@@ -43,7 +43,10 @@ use crate::shortcuts::{lookup, InputEvent, Shortcuts};
 pub const SUPPORTED_EXTENSIONS: &'static [&'static str] = &[
     "bmp", "dds", "exr", "ff", "gif", "hdr", "ico", "jpeg", "jpg", "png", "pnm", "psd", "svg",
     "tga", "tif", "tiff", "webp", "nef", "cr2", "dng", "mos", "erf", "raf", "arw", "3fr", "ari",
-    "srf", "sr2", "braw", "r3d", "nrw", "raw", "avif", "jxl",
+    "srf", "sr2", "braw", "r3d", "nrw", "raw", "avif",
+    
+    #[cfg(feature = "jpgxl")]
+    "jxl",
 ];
 
 fn is_pixel_fully_transparent(p: &Rgba<u8>) -> bool {
