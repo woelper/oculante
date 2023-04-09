b3f8025abb7afb4c56e87aebfb49ff678a789318
Johann Woelper <woelper@gmail.com>
2023-04-09 19:03:30 +0200

    build universal binary on release

--- build_mac.sh
+++ build_mac.sh
@@ -1,5 +1,8 @@
 export MACOSX_DEPLOYMENT_TARGET=10.15
 cargo bundle --release
-#cargo bundle
+# build universal binary
+cargo universal2
+# copy universal binary into .app
+cp target/universal2-apple-darwin/release/oculante target/release/bundle/osx/oculante.app/Contents/MacOS/oculante
 #cp res/info.plist target/debug/bundle/osx/oculante.app/Contents/Info.plist
 cp Info.plist target/release/bundle/osx/oculante.app/Contents/Info.plist
