58e6103a40b359598b839877a888c7868a4a3509
Johann Woelper <woelper@gmail.com>
2023-06-17 23:23:38 +0200

    Enable support for Apple Silicon (aarch64)

--- build_mac.sh
+++ build_mac.sh
@@ -1,6 +1,7 @@
 export MACOSX_DEPLOYMENT_TARGET=10.15
 cargo bundle --release
 # build universal binary
+rustup target add aarch64-apple-darwin
 cargo universal2
 # copy universal binary into .app
 cp target/universal2-apple-darwin/release/oculante target/release/bundle/osx/oculante.app/Contents/MacOS/oculante
