b8639fec18f93cd0a0335329dcb0081f06a6bf9b
Johann Woelper <woelper@gmail.com>
2023-08-21 22:28:40 +0200

    update release script

--- create_release.sh
+++ create_release.sh
@@ -8,7 +8,8 @@ then
     exit
 fi
 
-echo "You are on $branch, releasing!"
+VERSION=`cargo pkgid | cut -d# -f2 | cut -d: -f2`
+echo "You are on $branch, releasing! $VERSION"
 cargo install cargo-bump
 cargo install cargo-get
 cargo check --no-default-features
@@ -19,15 +20,15 @@ git add README.md
 git add Cargo.toml
 git add Cargo.lock
 git add PKGBUILD
-git commit -m "Release version `cargo get version`"
+git commit -m "Release version $VERSION"
 # tag the commit with current version
-git tag `cargo get version`
+git tag $VERSION
 # create changelog
-kokai release --ref `cargo get version` > tmp
+kokai release --ref $VERSION > tmp
 cat CHANGELOG.md >> tmp
 mv tmp CHANGELOG.md
 git add CHANGELOG.md
-git commit -m "Update changelog for `cargo get version`"
+git commit -m "Update changelog for $VERSION"
 git push --tags
 git push
 # this needs no-verify as we modify the plist during the build, and cargo does not accept that.
