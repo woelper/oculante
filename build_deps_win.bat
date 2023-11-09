4425f8117272941dec81ae1964f6421a01325e98
Johann Woelper <woelper@gmail.com>
2023-11-09 22:26:23 +0100

    change win heif buildscript

--- build_deps_win.bat
+++ build_deps_win.bat
@@ -1,6 +1,9 @@
-git clone https://github.com/Microsoft/vcpkg.git
-cd vcpkg
-./bootstrap-vcpkg.bat
-./vcpkg integrate install
-./vcpkg install libheif
-cd ..
+vcpkg install libde265:x64-windows 
+vcpkg install libheif:x64-windows
+
+@REM git clone https://github.com/Microsoft/vcpkg.git
+@REM cd vcpkg
+@REM ./bootstrap-vcpkg.bat
+@REM ./vcpkg integrate install
+@REM ./vcpkg install libheif
+@REM cd ..
