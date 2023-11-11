835de4359f2e3807d9f174bb4f14ecdee7e267a3
Johann Woelper <woelper@gmail.com>
2023-11-11 10:09:58 +0100

    different libheif install

--- build_deps_win.bat
+++ build_deps_win.bat
@@ -1,9 +1,9 @@
-vcpkg install libde265:x64-windows 
-vcpkg install libheif:x64-windows
+@REM vcpkg install libde265:x64-windows 
+@REM vcpkg install libheif:x64-windows
 
-@REM git clone https://github.com/Microsoft/vcpkg.git
-@REM cd vcpkg
-@REM ./bootstrap-vcpkg.bat
-@REM ./vcpkg integrate install
+git clone https://github.com/Microsoft/vcpkg.git
+cd vcpkg
+./bootstrap-vcpkg.bat
+./vcpkg integrate install
 @REM ./vcpkg install libheif
 @REM cd ..
