5d43b6cdea427fecd2fe48778595180c829c4946
Johann Woelper <woelper@gmail.com>
2023-11-12 22:33:16 +0100

    update mac buildscript

--- build_mac.sh
+++ build_mac.sh
@@ -1,4 +1,5 @@
 export MACOSX_DEPLOYMENT_TARGET=10.15
+cargo install cargo-bundle
 cargo bundle --release
 rustup target add aarch64-apple-darwin
 rustup target add x86_64-apple-darwin
@@ -13,7 +14,7 @@ lipo -create -output target/release/bundle/osx/oculante.app/Contents/MacOS/ocula
 # build universal binary
 # cargo universal2
 # copy universal binary into .app
-cp target/universal2-apple-darwin/release/oculante target/release/bundle/osx/oculante.app/Contents/MacOS/oculante
+# cp target/universal2-apple-darwin/release/oculante target/release/bundle/osx/oculante.app/Contents/MacOS/oculante
 #cp res/info.plist target/debug/bundle/osx/oculante.app/Contents/Info.plist
 cp Info.plist target/release/bundle/osx/oculante.app/Contents/Info.plist
 
