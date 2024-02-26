83daac971cf5fcaaea734cedb656a458bd9e8ffe
Johann Woelper <woelper@gmail.com>
2024-02-26 23:03:27 +0100

    don't build heif as default

--- build_mac.sh
+++ build_mac.sh
@@ -23,8 +23,8 @@ brew install nasm
 
 cargo build --release --target aarch64-apple-darwin
 cargo build --release --target x86_64-apple-darwin
-cargo build --release --target x86_64-apple-darwin --features heif
-cargo build --release --target aarch64-apple-darwin --features heif
+# cargo build --release --target x86_64-apple-darwin --features heif
+# cargo build --release --target aarch64-apple-darwin --features heif
 
 # cargo build --release --target x86_64-apple-darwin
 # cargo build --release --target aarch64-apple-darwin --features heif
