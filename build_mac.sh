0e1a3d7814b5066afe9b02427c5798103e4ea960
Johann Woelper <woelper@gmail.com>
2024-09-25 22:55:34 +0200

    feat: Heif support in OSX release builds

--- build_mac.sh
+++ build_mac.sh
@@ -4,38 +4,29 @@ rustup target list | grep installed
 export MACOSX_DEPLOYMENT_TARGET=10.15
 cargo install cargo-bundle
 cargo bundle --release --features notan/shaderc
+mkdir target/release/bundle/osx/oculante.app/Contents/Frameworks/
 rustup target add aarch64-apple-darwin
 rustup target add x86_64-apple-darwin
 brew install nasm
+brew install libheif
+cp /opt/homebrew/opt/libheif/lib/libheif.1.dylib target/release/bundle/osx/oculante.app/Contents/Frameworks/
 # arch -x86_64 brew install nasm
 
 
-# rm -rf libheif
-# git clone https://github.com/strukturag/libheif.git
-# cd libheif
-# mkdir build
-# cd build
-# cmake --preset=release ..
-# make install
-# cd ..
-# cd ..
 
 
-cargo build --release --target aarch64-apple-darwin --features notan/shaderc
+
+cargo build --release --target aarch64-apple-darwin --features notan/shaderc --features heif
+install_name_tool -change /opt/homebrew/opt/libheif/lib/libheif.1.dylib "@executable_path/../Frameworks/libheif.1.dylib" target/aarch64-apple-darwin/release/oculante
+# install_name_tool -change /opt/homebrew/opt/libheif/lib/libheif.1.dylib "@executable_path/../Frameworks/libheif.1.dylib" target/release/bundle/osx/oculante.app/Contents/oculante
 cargo build --release --target x86_64-apple-darwin --features notan/shaderc
-# cargo build --release --target x86_64-apple-darwin --features heif
-# cargo build --release --target aarch64-apple-darwin --features heif
-
-# cargo build --release --target x86_64-apple-darwin
-# cargo build --release --target aarch64-apple-darwin --features heif
+echo otool for aarch64:
+otool -L target/aarch64-apple-darwin/release/oculante
 lipo -create -output target/release/bundle/osx/oculante.app/Contents/MacOS/oculante target/x86_64-apple-darwin/release/oculante target/aarch64-apple-darwin/release/oculante 
 file target/release/bundle/osx/oculante.app/Contents/MacOS/oculante
+echo otool for universal binary:
+otool -L target/release/bundle/osx/oculante.app/Contents/MacOS/oculante
 
-# build universal binary
-# cargo universal2
-# copy universal binary into .app
-# cp target/universal2-apple-darwin/release/oculante target/release/bundle/osx/oculante.app/Contents/MacOS/oculante
-#cp res/info.plist target/debug/bundle/osx/oculante.app/Contents/Info.plist
 cp Info.plist target/release/bundle/osx/oculante.app/Contents/Info.plist
 
 
