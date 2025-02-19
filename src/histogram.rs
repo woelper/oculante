e6353b794531c1de2cf2cf339f5e190e8e366acf
B-LechCode <31586281+B-LechCode@users.noreply.github.com>
2025-02-20 00:59:06 +0100

    Reformat

--- src/histogram.rs
+++ src/histogram.rs
@@ -8,13 +8,13 @@ pub struct ImageStatistics<A: Clone + Debug> {
     pub hist_value: Vec<f32>,
     pub distinct_colors: u64,
     pub transparent_pixels: u64,
-    pub max_value:f32,
-    pub min_value:f32
+    pub max_value: f32,
+    pub min_value: f32,
 }
 
-trait ArgumentReducer<A, V> {    
-    fn reduce(&self, a: A, min:A, max:A, range:A) -> V;
-    fn bins(&self, min:f32, max:f32, range:f32) -> Vec<f32>;
+trait ArgumentReducer<A, V> {
+    fn reduce(&self, a: A, min: A, max: A, range: A) -> V;
+    fn bins(&self, min: f32, max: f32, range: f32) -> Vec<f32>;
 }
 
 #[derive(Clone, Default)]
@@ -34,18 +34,18 @@ struct ArgumentReducerFloat32ToU8 {}
 
 impl ArgumentReducer<u8, u8> for ArgumentReducerUnsigned8 {
     #[inline(always)]
-    fn reduce(&self, a: u8, _min:u8, _max:u8, _range:u8) -> u8 {
+    fn reduce(&self, a: u8, _min: u8, _max: u8, _range: u8) -> u8 {
         a
     }
 
-    fn bins(&self, _min:f32, _max:f32, _range:f32) -> Vec<f32> {        
-        (0..u8::MAX as i32+1).map(|f|{f as f32}).collect()
+    fn bins(&self, _min: f32, _max: f32, _range: f32) -> Vec<f32> {
+        (0..u8::MAX as i32 + 1).map(|f| f as f32).collect()
     }
 }
 
 impl<const BIT_DEPTH: usize> ArgumentReducer<u16, u16> for ArgumentReducerUnsigned16<BIT_DEPTH> {
     #[inline(always)]
-    fn reduce(&self, a: u16, _min:u16, _max:u16, _range:u16) -> u16 {
+    fn reduce(&self, a: u16, _min: u16, _max: u16, _range: u16) -> u16 {
         if BIT_DEPTH == 16 {
             a
         } else {
@@ -54,78 +54,82 @@ impl<const BIT_DEPTH: usize> ArgumentReducer<u16, u16> for ArgumentReducerUnsign
         }
     }
 
-    fn bins(&self, _min:f32, _max:f32, _range:f32) -> Vec<f32> {        
-        (0..1<<BIT_DEPTH).map(|f|{f as f32}).collect()
+    fn bins(&self, _min: f32, _max: f32, _range: f32) -> Vec<f32> {
+        (0..1 << BIT_DEPTH).map(|f| f as f32).collect()
     }
 }
 
 impl ArgumentReducer<f32, u16> for ArgumentReducerFloat32 {
     #[inline(always)]
-    fn reduce(&self, a: f32, min:f32, _max:f32, range:f32) -> u16 {
-        ((a-min)/range * (u16::MAX as f32)) as u16
+    fn reduce(&self, a: f32, min: f32, _max: f32, range: f32) -> u16 {
+        ((a - min) / range * (u16::MAX as f32)) as u16
         //(a * (u16::MAX as f32)) as u16
     }
-    fn bins(&self, min:f32, _max:f32, range:f32) -> Vec<f32> {  
-        let norm_factor = range/(u16::MAX as f32);      
-        (0..u16::MAX as i32+1).map(|f|{min+f as f32 * norm_factor}).collect()
+    fn bins(&self, min: f32, _max: f32, range: f32) -> Vec<f32> {
+        let norm_factor = range / (u16::MAX as f32);
+        (0..u16::MAX as i32 + 1)
+            .map(|f| min + f as f32 * norm_factor)
+            .collect()
     }
 }
 
 impl<const BIT_DEPTH: usize> ArgumentReducer<u16, u8> for ArgumentReducerUnsigned16ToU8<BIT_DEPTH> {
     #[inline(always)]
-    fn reduce(&self, a: u16, _min:u16, _max:u16, _range:u16) -> u8 {
+    fn reduce(&self, a: u16, _min: u16, _max: u16, _range: u16) -> u8 {
         a as u8
     }
-    fn bins(&self, _min:f32, _max:f32, _range:f32) -> Vec<f32> {        
-        (0..u8::MAX as i32+1).map(|f|{f as f32}).collect()
+    fn bins(&self, _min: f32, _max: f32, _range: f32) -> Vec<f32> {
+        (0..u8::MAX as i32 + 1).map(|f| f as f32).collect()
     }
 }
 
 impl ArgumentReducer<f32, u8> for ArgumentReducerFloat32ToU8 {
     #[inline(always)]
-    fn reduce(&self, a: f32, min:f32, _max:f32, range:f32) -> u8 {
-        ((a-min)/range * (u8::MAX as f32)) as u8
+    fn reduce(&self, a: f32, min: f32, _max: f32, range: f32) -> u8 {
+        ((a - min) / range * (u8::MAX as f32)) as u8
         //((a) * 255.) as u8
     }
 
-    fn bins(&self, min:f32, _max:f32, range:f32) -> Vec<f32> {  
-        let norm_factor = range/(u8::MAX as f32);      
-        (0..u8::MAX as i32+1).map(|f|{min+f as f32 * norm_factor}).collect()
+    fn bins(&self, min: f32, _max: f32, range: f32) -> Vec<f32> {
+        let norm_factor = range / (u8::MAX as f32);
+        (0..u8::MAX as i32 + 1)
+            .map(|f| min + f as f32 * norm_factor)
+            .collect()
     }
 }
 
-trait AlphaTest{ 
-    fn is_transparent_alpha(self)-> bool;
-    }
+trait AlphaTest {
+    fn is_transparent_alpha(self) -> bool;
+}
 
 impl AlphaTest for u8 {
     #[inline(always)]
-    fn is_transparent_alpha(self)-> bool{
+    fn is_transparent_alpha(self) -> bool {
         self == 0
     }
 }
 
 impl AlphaTest for u16 {
     #[inline(always)]
-    fn is_transparent_alpha(self)-> bool{
+    fn is_transparent_alpha(self) -> bool {
         self == 0
     }
 }
 
 impl AlphaTest for f32 {
     #[inline(always)]
-    fn is_transparent_alpha(self)-> bool{
+    fn is_transparent_alpha(self) -> bool {
         let mut value = self;
 
         if self.is_nan() {
-            value= f32::MAX // Or any local positive extremum, as 1, 255, 65535 etc
+            value = f32::MAX // Or any local positive extremum, as 1, 255, 65535 etc
         } else if self.is_infinite() && self.is_positive() {
-            value= f32::MAX // Or any local positive extremum, as 1, 255, 65535 etc
+            value = f32::MAX // Or any local positive extremum, as 1, 255, 65535 etc
         } else if self.is_infinite() && self.is_negative() {
-            value= f32::MIN  // Or any local negative extremum, 0 if u8, u16 type required
+            value = f32::MIN // Or any local negative extremum, 0 if u8, u16 type required
         } else if self.is_subnormal() {
-            value= 0. // Non-transparent value, since 1/65535 is subnormal, but for 16 bit-depth it is not transparent :)
-        } 
+            value = 0. // Non-transparent value, since 1/65535 is subnormal, but for 16 bit-depth it is not transparent :)
+        }
 
         value <= f32::EPSILON
     }
@@ -137,22 +141,22 @@ pub trait MinMax {
     const RANGE_MIN: Self;
     const RANGE_MAX: Self;
 
-    fn mini(self, b:Self)-> Self;
-    fn maxi(self, b:Self)-> Self;
+    fn mini(self, b: Self) -> Self;
+    fn maxi(self, b: Self) -> Self;
 }
 
 impl MinMax for u8 {
     const MIN: u8 = u8::MIN;
     const MAX: u8 = u8::MAX;
-    const RANGE_MIN:u8 = 0u8;
-    const RANGE_MAX:u8 = Self::MAX;
+    const RANGE_MIN: u8 = 0u8;
+    const RANGE_MAX: u8 = Self::MAX;
 
     #[inline(always)]
-    fn mini(self, b:u8)-> u8{
+    fn mini(self, b: u8) -> u8 {
         self.min(b)
     }
     #[inline(always)]
-    fn maxi(self, b:u8)-> u8{
+    fn maxi(self, b: u8) -> u8 {
         self.max(b)
     }
 }
@@ -160,14 +164,14 @@ impl MinMax for u8 {
 impl MinMax for u16 {
     const MIN: u16 = u16::MIN;
     const MAX: u16 = u16::MAX;
-    const RANGE_MIN:u16 = 0u16;
-    const RANGE_MAX:u16 = Self::MAX;
+    const RANGE_MIN: u16 = 0u16;
+    const RANGE_MAX: u16 = Self::MAX;
     #[inline(always)]
-    fn mini(self, b:u16)-> u16{
+    fn mini(self, b: u16) -> u16 {
         self.min(b)
     }
     #[inline(always)]
-    fn maxi(self, b:u16)-> u16{
+    fn maxi(self, b: u16) -> u16 {
         self.max(b)
     }
 }
@@ -175,18 +179,17 @@ impl MinMax for u16 {
 impl MinMax for f32 {
     const MIN: f32 = f32::MIN;
     const MAX: f32 = f32::MAX;
-    const RANGE_MIN:f32 = 0.0f32;
-    const RANGE_MAX:f32 = 1.0f32;
+    const RANGE_MIN: f32 = 0.0f32;
+    const RANGE_MAX: f32 = 1.0f32;
     #[inline(always)]
-    fn mini(self, b:f32)-> f32{
+    fn mini(self, b: f32) -> f32 {
         self.min(b)
     }
     #[inline(always)]
-    fn maxi(self, b:f32)-> f32{
+    fn maxi(self, b: f32) -> f32 {
         self.max(b)
     }
 }
-
 
 fn calculate_statistics_impl<
     A: Clone + Debug + Copy + AlphaTest + MinMax + Into<f32> + std::ops::Sub<Output = A>,
@@ -194,7 +197,7 @@ fn calculate_statistics_impl<
     const CN: usize,
     const USEFUL_CN: usize,
     const BINS_DEPTH: usize,
-    const USE_MIN_MAX: bool
+    const USE_MIN_MAX: bool,
 >(
     image: &[A],
     stride: usize,
@@ -214,9 +217,9 @@ where
     //Calculate min and max
     let mut max_value: A = A::MIN;
     let mut min_value: A = A::MAX;
-    for chunk in image.chunks_exact(CN){
-        for i in 0..USEFUL_CN{
-            max_value = max_value.maxi(chunk[i]);            
+    for chunk in image.chunks_exact(CN) {
+        for i in 0..USEFUL_CN {
+            max_value = max_value.maxi(chunk[i]);
             min_value = min_value.mini(chunk[i]);
         }
     }
@@ -224,26 +227,25 @@ where
     let min_scale: A;
     let max_scale: A;
     let range_scale: A;
-    if(USE_MIN_MAX){
-        min_scale = min_value;
-        max_scale = max_value;
-    }
-    else {
+    if USE_MIN_MAX {
+        min_scale = min_value.mini(A::RANGE_MIN);
+        max_scale = max_value.maxi(A::RANGE_MAX);
+    } else {
         min_scale = A::RANGE_MIN;
-        max_scale = A::RANGE_MAX;        
+        max_scale = A::RANGE_MAX;
     }
-    range_scale = max_scale-min_scale;
+    range_scale = max_scale - min_scale;
 
     let elapsed = now.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
-    
+
     let now2 = Instant::now();
     let bins_count = 1 << BINS_DEPTH;
     let mut hist_bins = vec![];
-    for _i in 0..USEFUL_CN{
+    for _i in 0..USEFUL_CN {
         hist_bins.push(vec![0u64; bins_count]);
-    }    
-    
+    }
+
     /*let mut bin0 = vec![0u64; bins_count];
     let mut bin1 = if USEFUL_CN > 1 {
         vec![0u64; bins_count]
@@ -257,7 +259,7 @@ where
     };*/
 
     let mut transparent_pixels: u64 = 0;
-    let has_alpha = (CN-USEFUL_CN) == 1;
+    let has_alpha = (CN - USEFUL_CN) == 1;
 
     let mut asdf = [0u8; 4];
     //let mut distinct_map = vec![0u8; 1 << 21];
@@ -268,34 +270,36 @@ where
     const SUB_INDEX_SIZE: usize = 5;
     const MAIN_INDEX_SIZE: usize = 1 << (FIXED_RGB_SIZE - SUB_INDEX_SIZE);
     let mut color_map = vec![0u32; MAIN_INDEX_SIZE];
-    for chunk in image.chunks_exact(CN){
+    for chunk in image.chunks_exact(CN) {
         let mut trans = true;
-        
-        for i in 0..USEFUL_CN{
-            let c: usize = reducer.reduce(chunk[i], min_scale, max_scale, range_scale).as_();
+
+        for i in 0..USEFUL_CN {
+            let c: usize = reducer
+                .reduce(chunk[i], min_scale, max_scale, range_scale)
+                .as_();
             hist_bins[i][c] += 1;
             trans &= chunk[i].is_transparent_alpha();
 
-            let col_reduced: u8 = distinct_colors_reducer.reduce(chunk[i], min_scale, max_scale, range_scale);
+            let col_reduced: u8 =
+                distinct_colors_reducer.reduce(chunk[i], min_scale, max_scale, range_scale);
             asdf[i] = col_reduced;
         }
-        if has_alpha{                
+        if has_alpha {
             trans &= chunk[USEFUL_CN].is_transparent_alpha();
-            if trans{
+            if trans {
                 transparent_pixels += 1;
             }
-            
-        }            
+        }
 
         let pos: u32 = u32::from_ne_bytes(asdf);
-                /*let full: u32 = v_u32 >> 3;
-                let remainder = v_u32 - (full << 3);
+        /*let full: u32 = v_u32 >> 3;
+        let remainder = v_u32 - (full << 3);
 
-                let bit = 1 << remainder;
-                distinct_map[full as usize] |= bit;*/
-            let pos_main = pos >> SUB_INDEX_SIZE;
-            let pos_sub = pos - (pos_main << SUB_INDEX_SIZE);
-            color_map[pos_main as usize] |= 1 << pos_sub;
+        let bit = 1 << remainder;
+        distinct_map[full as usize] |= bit;*/
+        let pos_main = pos >> SUB_INDEX_SIZE;
+        let pos_sub = pos - (pos_main << SUB_INDEX_SIZE);
+        color_map[pos_main as usize] |= 1 << pos_sub;
     }
     let elapsed = now2.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
@@ -319,7 +323,7 @@ where
             if USEFUL_CN > 2 {
                 dst[2] = reducer.reduce(src[2]);
             }
-            
+
         }
 
         // Calculate hist
@@ -343,11 +347,11 @@ where
 
     /*working_row.resize(0, V::default());
 
-    
+
 
     let mut d_working_row = vec![0u8; USEFUL_CN * width];
 
-    
+
 
     for row in image.chunks_exact(stride) {
         let fixed_row = &row[0..width * CN];
