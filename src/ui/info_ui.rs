432519289057ee4be96b848f85c49ef82bc69fc0
Johann Woelper <woelper@gmail.com>
2025-11-13 22:32:51 +0100

    use info ui from master

--- src/ui/info_ui.rs
+++ src/ui/info_ui.rs
@@ -7,7 +7,6 @@ use crate::utils::*;
 use egui_plot::{Line, Plot, PlotPoints};
 use image::ColorType;
 
-use notan::egui::style::ScrollStyle;
 #[cfg(not(any(target_os = "netbsd", target_os = "freebsd")))]
 use notan::{
     egui::{self, *},
@@ -17,9 +16,11 @@ use notan::{
 use super::*;
 use std::time::Duration;
 
-pub fn info_ui(ctx: &Context, state: &mut OculanteState, _gfx: &mut Graphics) -> (Rect, f32) {
+pub fn info_ui(ctx: &Context, state: &mut OculanteState, _gfx: &mut Graphics) -> (Pos2, Pos2) {
     let mut color_type = ColorType::Rgba8;
-    let mut bbox = Rect::NOTHING;
+    let mut bbox_tl: Pos2 = Default::default();
+    let mut bbox_br: Pos2 = Default::default();
+    let mut uv_center: (f64, f64) = Default::default();
     let mut uv_size: (f64, f64) = Default::default();
 
     if let Some(img) = &state.current_image {
@@ -33,7 +34,7 @@ pub fn info_ui(ctx: &Context, state: &mut OculanteState, _gfx: &mut Graphics) ->
         };
 
         // don't do this every frame for performance reasons
-        if ctx.cumulative_pass_nr() % 5 == 0 {
+        if ctx.frame_nr() % 5 == 0 {
             if let Some(p) = get_pixel_checked(
                 img,
                 state.cursor_relative.x as u32,
@@ -44,59 +45,63 @@ pub fn info_ui(ctx: &Context, state: &mut OculanteState, _gfx: &mut Graphics) ->
         }
     }
 
-    let sp = egui::SidePanel::left("info")
-        .show_separator_line(false)
-        .min_width(PANEL_WIDTH)
-        .frame(
-            egui::Frame::central_panel(&ctx.style())
-                .corner_radius(CornerRadius::ZERO)
-                .fill(Color32::from_rgba_unmultiplied(100, 100, 0, 0)),
-        )
-        .show(ctx, |ui| {
-            ui.style_mut().spacing.scroll = ScrollStyle::thin();
+    egui::SidePanel::left("info")
+    .show_separator_line(false)
+    .exact_width(PANEL_WIDTH)
+    .resizable(false)
+    .frame(egui::Frame::central_panel(&ctx.style()).rounding(Rounding::ZERO).fill(Color32::TRANSPARENT))
+    .show(ctx, |ui| {
+        egui::ScrollArea::vertical().auto_shrink([false,true])
+            .show(ui, |ui| {
 
-            egui::ScrollArea::vertical()
-                .auto_shrink([true, true])
-                .scroll_bar_visibility(scroll_area::ScrollBarVisibility::AlwaysVisible)
-                // .scroll_bar_visibility(scroll_area::ScrollBarVisibility::VisibleWhenNeeded)
-                
-                .show(ui, |ui| {
-           
-                    info_grid(ui, state, color_type);
+            // Force-expand to prevent spacing issue with scroll bar
+            ui.allocate_space(egui::Vec2::new(PANEL_WIDTH - PANEL_WIDGET_OFFSET, 0.));
 
-                    if let Some(texture) = &state.current_texture.get() {
-                        let desired_width = PANEL_WIDTH as f64 - PANEL_WIDGET_OFFSET as f64;
-                        let scale = (desired_width / 8.) / texture.size().0 as f64;
-        
-                        // make sure aspect ratio is compensated for the square preview
-                        let ratio = texture.size().0 as f64 / texture.size().1 as f64;
-                        uv_size = (scale, scale * ratio);
-                        let preview_rect = egui::Rect::from_min_size(
-                            ui.cursor().left_top(),
-                            egui::Vec2::splat(ui.available_width()),
-                        );
-                        bbox = preview_rect;
-                        ui.advance_cursor_after_rect(preview_rect);
+            if let Some(texture) = &state.current_texture.get() {
+                let desired_width = PANEL_WIDTH as f64 - PANEL_WIDGET_OFFSET as f64;
+                let scale = (desired_width / 8.) / texture.size().0 as f64;
+                uv_center = (
+                    state.cursor_relative.x as f64 / state.image_geometry.dimensions.0 as f64,
+                    (state.cursor_relative.y as f64 / state.image_geometry.dimensions.1 as f64),
+                );
+
+                egui::Grid::new("info")
+                    .num_columns(2)
+                    .show(ui, |ui| {
+                    ui.label_i(format!("{ARROWS_OUT} Size",));
+                    ui.label_right(
+                        RichText::new(format!(
+                            "{}x{}",
+                            state.image_geometry.dimensions.0, state.image_geometry.dimensions.1
+                        ))
+                    );
+                    ui.end_row();
+
+                    if let Some(path) = &state.current_path {
+                        // make sure we truncate filenames
+                        let file_name = path.file_name().unwrap_or_default().to_string_lossy();
+                        ui.label_i(format!("{} File", IMAGE));
+                        let path_label = egui::Label::new(
+                            RichText::new(file_name)
+                        ).truncate(true);
+                        ui.with_layout(egui::Layout::right_to_left(Align::Center), |ui| {
+                            ui.add(path_label)
+                            .on_hover_text(format!("{}", path.display()));
+                        });
+                        ui.end_row();
                     }
-                    
 
-                    ui.styled_collapsing("Compare", |ui| {
-                        if state.persistent_settings.max_cache == 0 {
-                            ui.label("Warning! Set your cache to more than 0 in settings for this to be fast.");
-                        }
-                        ui.vertical_centered_justified(|ui| {
-                            dark_panel(ui, |ui| {
-                                if ui.button(format!("{FOLDER} Open another image...")).clicked() {
-                                    // TODO: Automatically insert image into compare list
-                                    #[cfg(feature = "file_open")]
-                                    browse_for_image_path(state);
-                                    #[cfg(not(feature = "file_open"))]
-                                    ui.ctx().memory_mut(|w| w.open_popup(Id::new("OPEN")));
+                    ui.label_i(format!("{PALETTE} RGBA"));
+                    ui.label_right(
+                        RichText::new(disp_col(state.sampled_color))
+                    );
+                    ui.end_row();
 
-                                    state.is_loaded = false;
-                                    // tag to add new image
-                                    ui.ctx().data_mut(|w|w.insert_temp("compare".into(), true));
-                                }
+                    ui.label_i(format!("{PALETTE} RGBA"));
+                    ui.label_right(
+                        RichText::new(disp_col_norm(state.sampled_color, 255.))
+                    );
+                    ui.end_row();
 
                     ui.label_i(format!("{PALETTE} HEX"));
                     let hex = Color32::from_rgba_unmultiplied(state.sampled_color[0] as u8, state.sampled_color[1] as u8, state.sampled_color[2] as u8, state.sampled_color[3] as u8).to_hex();
@@ -141,7 +146,7 @@ pub fn info_ui(ctx: &Context, state: &mut OculanteState, _gfx: &mut Graphics) ->
                 bbox_tl = preview_rect.left_top();
                 bbox_br = preview_rect.right_bottom();
                 ui.advance_cursor_after_rect(preview_rect);
-            });
+            }
             ui.add_space(10.);
             ui.vertical_centered_justified(|ui| {
                 ui.styled_collapsing("Compare", |ui| {
@@ -187,16 +192,6 @@ pub fn info_ui(ctx: &Context, state: &mut OculanteState, _gfx: &mut Graphics) ->
                                             ui.ctx().request_repaint_after(Duration::from_millis(500));
                                             state.current_path = Some(path.clone());
                                         }
-                                        ui.vertical_centered_justified(|ui| {
-                                            if ui.selectable_label(state.current_path.as_ref() == Some(&path), path.file_name().map(|f| f.to_string_lossy().to_string()).unwrap_or_default().to_string()).clicked(){
-                                                state
-                                                    .player
-                                                    .load_advanced(&path, Some(crate::utils::Frame::CompareResult(Default::default(), geo.clone())));
-                                                ui.ctx().request_repaint();
-                                                ui.ctx().request_repaint_after(Duration::from_millis(500));
-                                                state.current_path = Some(path);
-                                            }
-                                        });
                                     });
                                 });
                             }
@@ -219,130 +214,64 @@ pub fn info_ui(ctx: &Context, state: &mut OculanteState, _gfx: &mut Graphics) ->
                             }
                         });
                     });
+                });
+            });
 
-                    if state.current_texture.get().is_some() {
-                        ui.styled_collapsing("Alpha tools", |ui| {
-                            ui.vertical_centered_justified(|ui| {
-                                dark_panel(ui, |ui| {
-                                    if let Some(img) = &state.current_image {
-                                        if ui
-                                            .button("Show alpha bleed")
-                                            .on_hover_text("Highlight pixels with zero alpha and color information")
-                                            .clicked()
-                                        {
-                                            state.edit_state.result_pixel_op = highlight_bleed(img);
-                                            state.send_frame(crate::utils::Frame::UpdateTexture);
-                                            ui.ctx().request_repaint();
-                                        }
-                                        if ui
-                                            .button("Show semi-transparent pixels")
-                                            .on_hover_text(
-                                                "Highlight pixels that are neither fully opaque nor fully transparent",
-                                            )
-                                            .clicked()
-                                        {
-                                            state.edit_state.result_pixel_op = highlight_semitrans(img);
-                                            state.send_frame(crate::utils::Frame::UpdateTexture);
-                                            ui.ctx().request_repaint();
-                                        }
-                                        if ui.button("Reset image").clicked() {
-                                            state.edit_state.result_pixel_op = Default::default();
+            if state.current_texture.get().is_some() {
+                ui.styled_collapsing("Alpha tools", |ui| {
+                    ui.vertical_centered_justified(|ui| {
+                        dark_panel(ui, |ui| {
+                            if let Some(img) = &state.current_image {
+                                if ui
+                                    .button("Show alpha bleed")
+                                    .on_hover_text("Highlight pixels with zero alpha and color information")
+                                    .clicked()
+                                {
+                                    state.edit_state.result_pixel_op = highlight_bleed(img);
+                                    state.send_frame(crate::utils::Frame::UpdateTexture);
+                                    ui.ctx().request_repaint();
+                                }
+                                if ui
+                                    .button("Show semi-transparent pixels")
+                                    .on_hover_text(
+                                        "Highlight pixels that are neither fully opaque nor fully transparent",
+                                    )
+                                    .clicked()
+                                {
+                                    state.edit_state.result_pixel_op = highlight_semitrans(img);
+                                    state.send_frame(crate::utils::Frame::UpdateTexture);
+                                    ui.ctx().request_repaint();
+                                }
+                                if ui.button("Reset image").clicked() {
+                                    state.edit_state.result_pixel_op = Default::default();
 
-                                            state.send_frame(crate::utils::Frame::UpdateTexture);
-                                        }
-                                    }
-                                });
-                            });
+                                    state.send_frame(crate::utils::Frame::UpdateTexture);
+                                }
+                            }
                         });
-
-                        palette_ui(ui, state);
-
-                        if state.persistent_settings.experimental_features {
-                            measure_ui(ui, state);
-                        }
-
-                        ui.horizontal(|ui| {
-                            ui.label("Tiling");
-                            ui.style_mut().spacing.slider_width = ui.available_width() - 16.;
-                            ui.styled_slider(&mut state.tiling, 1..=10);
-                        });
-                    }
-                    advanced_ui(ui, state);
+                    });
                 });
 
-                
+                palette_ui(ui, state);
+
+                if state.persistent_settings.experimental_features {
+                    measure_ui(ui, state);
+                }
+
+                ui.horizontal(|ui| {
+                    ui.label("Tiling");
+                    ui.style_mut().spacing.slider_width = ui.available_width() - 16.;
+                    ui.styled_slider(&mut state.tiling, 1..=10);
+                });
+            }
+
+            advanced_ui(ui, state);
+
         });
-
-    (bbox, sp.response.rect.width())
-}
-
-fn info_grid(ui: &mut Ui, state: &mut OculanteState, color_type: ColorType) {
-    let uv_center        = (
-        state.cursor_relative.x as f64
-            / state.image_geometry.dimensions.0 as f64,
-        (state.cursor_relative.y as f64
-            / state.image_geometry.dimensions.1 as f64),
-    );
-    
-    egui::Grid::new("info").num_columns(2).show(ui, |ui| {
-        ui.label_i(format!("{ARROWS_OUT} Size",));
-        ui.label_right(RichText::new(format!(
-            "{}x{}",
-            state.image_geometry.dimensions.0, state.image_geometry.dimensions.1
-        )));
-        ui.end_row();
-
-        if let Some(path) = &state.current_path {
-            // make sure we truncate filenames
-            let file_name = path.file_name().unwrap_or_default().to_string_lossy();
-            ui.label_i(format!("{} File", IMAGE));
-            let path_label = egui::Label::new(RichText::new(file_name)).truncate();
-            ui.with_layout(egui::Layout::right_to_left(Align::Center), |ui| {
-                ui.add(path_label)
-                    .on_hover_text(format!("{}", path.display()));
-            });
-            ui.end_row();
-        }
-
-        ui.label_i(format!("{PALETTE} RGBA"));
-        ui.label_right(RichText::new(disp_col(state.sampled_color)));
-        ui.end_row();
-
-        ui.label_i(format!("{PALETTE} RGBA"));
-        ui.label_right(RichText::new(disp_col_norm(state.sampled_color, 255.)));
-        ui.end_row();
-
-        ui.label_i(format!("{PALETTE} HEX"));
-        let hex = Color32::from_rgba_unmultiplied(
-            state.sampled_color[0] as u8,
-            state.sampled_color[1] as u8,
-            state.sampled_color[2] as u8,
-            state.sampled_color[3] as u8,
-        )
-        .to_hex();
-        ui.label_right(RichText::new(hex));
-        ui.end_row();
-
-        ui.label_i(format!("{PALETTE} Color"));
-        ui.label_right(format!("{:?}", color_type));
-        ui.end_row();
-
-        ui.label_i(&format!("{MOVE} Pos"));
-        ui.label_right(RichText::new(format!(
-            "{:.0},{:.0}",
-            state.cursor_relative.x, state.cursor_relative.y
-        )));
-        ui.end_row();
-
-        ui.label_i(format!("{INTERSECT} UV"));
-        ui.label_right(RichText::new(format!(
-            "{:.3},{:.3}",
-            uv_center.0,
-            1.0 - uv_center.1
-        )));
-        ui.end_row();
     });
+    (bbox_tl, bbox_br)
 }
+
 fn advanced_ui(ui: &mut Ui, state: &mut OculanteState) {
     if let Some(info) = &state.image_metadata {
         egui::Grid::new("extended").num_columns(2).show(ui, |ui| {
@@ -439,8 +368,7 @@ fn advanced_ui(ui: &mut Ui, state: &mut OculanteState) {
             .allow_drag(false)
             .show_axes(false)
             .show_grid(false)
-            // .width(50. - ui.style().spacing.scroll.bar_width)
-            // .width(PANEL_WIDTH - PANEL_WIDGET_OFFSET)
+            .width(PANEL_WIDTH - PANEL_WIDGET_OFFSET)
             .show(ui, |plot_ui| {
                 plot_ui.line(red_vals);
                 plot_ui.line(green_vals);
