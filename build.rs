3902313b1159ba45eb96a2b8849264443bab463c
Stoppedpuma <58333920+Stoppedpuma@users.noreply.github.com>
2026-05-19 17:08:50 +0200

    winres icon cleanup

--- build.rs
+++ build.rs
@@ -123,7 +123,7 @@ fn main() {
 
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("icon.ico");
+        res.set_icon("res/icons/icon.ico");
         _ = res.compile();
     }
 
